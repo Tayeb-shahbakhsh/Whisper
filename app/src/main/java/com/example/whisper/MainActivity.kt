1a8b7c8c2849a3883c96346772137753600f1a35
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-02-13 11:19:15 +0330

    user 1 image added to top bar

--- app/src/main/java/com/example/whisper/MainActivity.kt
+++ app/src/main/java/com/example/whisper/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.whisper
 
 import android.graphics.fonts.FontStyle
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -14,7 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.whisper.ui.theme.Shapes
@@ -44,14 +52,14 @@ fun MessagingUi(){
 
     TopAppBar(
         title = {
-            Text(text = "SemicolonSpace")
+            Text(text = "Ali Dehghanian")
         },
         navigationIcon = {
             IconButton(onClick = {
                 Toast.makeText(contextForToast, "Back Icon Click", Toast.LENGTH_SHORT)
                     .show()
             }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Go Back")
+                Image(modifier = Modifier.border(shape = RoundedCornerShape(20.dp)),painter = painterResource(id = R.drawable.user1), contentDescription = "user profile")
             }
         }
     )
